4e73ebe9fbc9786fa09b54a92692f42b558d76a0
N0tstgr <vikashkasaudhan321@gmail.com>
2025-09-08 00:09:48 +0530

    app.2

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+@app.route("/products")
+def products():
+    return "This is product page"
+
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
