c84473deedd16348ef16e403653032fe8ccdd218
N0tstgr <vikashkasaudhan321@gmail.com>
2025-09-11 05:06:41 +0530

    sqlalchemy

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask ,render_template
-
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///Not.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
+
+class Todo(db.Model):
+    sno = db.Coloumn(db.Integer, primary_key=True)
+    title
 @app.route("/")
 def hello_world():
     return render_template('main.html')
